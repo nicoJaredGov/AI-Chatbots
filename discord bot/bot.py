e268edca26b0234c1919c3007e115a7c65865d88
Nico Govindsamy <ngovindsamy18@gmail.com>
2023-04-09 20:52:12 +0200

    discord bot running

--- discord bot/bot.py
+++ discord bot/bot.py
@@ -17,4 +17,21 @@ def run_discord_bot():
     async def on_ready():
         print(f'{client.user} is now running!')
 
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: {user_message} in ({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
     client.run(TOKEN)
